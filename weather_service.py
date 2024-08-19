2131cbd1453a64d32d15fe43a17022dd83ff9719
Arseniy Glushkov <miguelprod8@gmail.com>
2024-08-20 03:29:31 +0700

    feat: adapted for non-ip-needed answer

--- weather_service.py
+++ weather_service.py
@@ -1,11 +1,15 @@
 from typing import NamedTuple
 from urllib import request
 import json
-from coordinates_service import Coordinates
 
+OPENMETEO_CURRENTWEATHER_URL = "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m"
+
+
+class Coordinates(NamedTuple):
+    latitude: set
+    longtitude: str
 
 Celsius = int
-
 
 class Weather(NamedTuple):
     temperature: Celsius
@@ -14,7 +18,8 @@ class Weather(NamedTuple):
 
 class OpenmeteoParcer:
 
-    _currentweatherurl = "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m"
+    def __init__(self) -> None:
+        self.currentweatherurl = OPENMETEO_CURRENTWEATHER_URL
 
     def get_weather(self, coordinates: Coordinates) -> Weather:
         url = self.__url_formatter(coordinates)
@@ -30,7 +35,7 @@ class OpenmeteoParcer:
     def __url_formatter(self, coordinates: Coordinates) -> str:
         latitude = coordinates.latitude
         longitude = coordinates.longtitude
-        formatted_url = self._currentweatherurl.format(latitude, longitude)
+        formatted_url = self.currentweatherurl.format(latitude, longitude)
         return formatted_url
 
     def __weather_formatter(self, raw_data: dict, city_name: str) -> Weather:
