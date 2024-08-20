50bc6d7f6c7307c52b69effe335aa4d052f34be1
Arseniy Glushkov <miguelprod8@gmail.com>
2024-08-20 16:03:51 +0700

    feat: refactoring for local work

--- server.py
+++ server.py
@@ -2,24 +2,23 @@ import socket as soc
 from socket import socket
 import select
 
-from weather_service import OpenmeteoParcer
-from coordinates_service import GetCoordinates
+from weather_service import OpenmeteoParcer, Coordinates
 
 
 class Server:
-    __tasks = []
-    __inputs = {}
-    __outputs = {}
+    _tasks = []
+    _inputs = {}
+    _outputs = {}
+
+    content = None
 
     def __init__(self, host: str, port: int) -> None:
         self.server_socket = soc.socket(soc.AF_INET, soc.SOCK_STREAM)
         self.server_socket.setsockopt(soc.SOL_SOCKET, soc.SO_REUSEADDR, 1)
         self.server_socket.bind((host, port))
         self.server_socket.listen()
-        self.__tasks.append(self.__server())
 
-        self.weather_parcer = OpenmeteoParcer()
-        self.coordinates_parcer = GetCoordinates()
+        self.weather_service = OpenmeteoParcer()
 
     def __parse_request(self, request: str):
         parsed = request.split(" ")
@@ -35,31 +34,30 @@ class Server:
 
         return ("HTTP/1.1 200 OK\n\n", 200)
 
-    def __generate_content(self, code, ip_address):
+    def __generate_content(self, code):
         if code == 404:
             return "<h1>404</h1><p>Not fount</p>"
         if code == 405:
             return "<h1>405</h1><p>Method not allowed</p>"
 
-        coordinates = self.coordinates_parcer.get_coordinates(ip_address)
-        weather = self.weather_parcer.get_weather(coordinates)
-        return "<h1>it's {} degrees Celsius in {} now.</h1>".format(
-            weather.temperature, weather.city
+        current_weather = self.weather_service.get_weather(
+            Coordinates(latitude=53.36056, longtitude=83.76361)
         )
+        return "<h1>it's {} degrees Celsius in Barnaul now.</h1>".format(current_weather.temperature)
 
-    def __generate_response(self, request, ip_address):
+    def __generate_response(self, request):
         method, url = self.__parse_request(request)
         headers, code = self.__generate_headers(method, url)
-        body = self.__generate_content(code, ip_address)
+        body = self.__generate_content(code)
         return (headers + body).encode()
 
-    def __server(self):
+    def _server(self):
         while True:
             yield ("read", self.server_socket)
             client_socket, addr = self.server_socket.accept()
-            self.__tasks.append(self.__client(client_socket, addr))
+            self._tasks.append(self.__client(client_socket))
 
-    def __client(self, client_socket: socket, addr):
+    def __client(self, client_socket: socket):
         while True:
             yield ("read", client_socket)
             request = client_socket.recv(1024)
@@ -68,33 +66,9 @@ class Server:
             if not request:
                 break
             else:
-                response = self.__generate_response(request.decode("utf-8"), addr[0])
+                response = self.__generate_response(request.decode("utf-8"))
 
                 yield ("write", client_socket)
                 client_socket.send(response)
 
         client_socket.close()
-
-    def event_loop(self):
-        while any([self.__tasks, self.__inputs, self.__outputs]):
-            while not self.__tasks:
-                ready_inputs, ready_outputs, _ = select.select(
-                    self.__inputs.keys(), self.__outputs.keys(), [], 0.5
-                )
-
-                for sock in ready_inputs:
-                    self.__tasks.append(self.__inputs.pop(sock))
-                for sock in ready_outputs:
-                    self.__tasks.append(self.__outputs.pop(sock))
-
-            try:
-                task = self.__tasks.pop(0)
-                method, sock = next(task)
-
-                if method == "read":
-                    self.__inputs[sock] = task
-                elif method == "write":
-                    self.__outputs[sock] = task
-
-            except StopIteration:
-                pass
